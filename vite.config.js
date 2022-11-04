41d18c32f5040c10e5d8a60810b652a9e4f00679
marius-lionte <mariusmihai.learning@gmail.com>
2022-11-04 16:19:44 +0200

    add test workflow

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.js',
-  },
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.js",
+    reporters: ["json", "junit"],
+    outputFile: {
+      json: "reports/report.json",
+      junit: "reports/report.xml"
+    }
+  }
 });
