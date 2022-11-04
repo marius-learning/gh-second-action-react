8841f787efa539bed957f41e5174b308e074ecc5
marius-lionte <mariusmihai.learning@gmail.com>
2022-11-04 15:34:00 +0200

    configure test reports and steps to display them

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.js',
-  },
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.js",
+    reporters: ["junit", "json"],
+
+    outputFile: {
+      json: "reports.json",
+      junit: "reports.xml"
+    }
+  }
 });
